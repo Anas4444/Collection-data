bf59d2e2107cc619bcef3620a19ac17624e262e8
anas4444 <anas.elabed@etudiant-fst.utm.tn>
2023-01-01 21:03:10 +0100

    t

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 
 var connection = mysql.createConnection({
-	host : 'localhost',
+	host : 'mydb.ckl1zkh9idqi.us-east-1.rds.amazonaws.com',
 	user : 'admin',
 	password : 'anas123456789!'
 });
