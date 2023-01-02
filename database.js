c78c181c190bba4f5092d6994297910794223601
anas4444 <anas.elabed@etudiant-fst.utm.tn>
2023-01-02 13:44:03 +0100

    c

--- database.js
+++ database.js
@@ -21,10 +21,16 @@ connection.connect(function(error){
 			connection.query("USE testing", function (err) {
 				if (err) throw err;
 				console.log("Database testing is being used");
-				var sql = "CREATE TABLE IF NOT EXISTS sample_data (id INT(10) NOT NULL, first_name VARCHAR(250) NOT NULL, last_name VARCHAR(250) NOT NULL, age VARCHAR(30) NOT NULL, gender VARCHAR(30) NOT NULL)";
-				connection.query(sql, function (err, result) {
+				var sql = "CREATE TABLE IF NOT EXISTS sample_data (id INT(10) NOT NULL PRIMARY KEY, first_name VARCHAR(250) NOT NULL, last_name VARCHAR(250) NOT NULL, age VARCHAR(30) NOT NULL, gender VARCHAR(30) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=latin1";
+				connection.query(sql, function (err) {
 					if (err) throw err;
 					console.log("Table created");
+					
+					var sqlalter = "ALTER TABLE sample_data MODIFY id int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5"
+					connection.query(sqlalter, function(err) {
+						if (err) throw err;
+						console.log("Table successfully modified");
+					})
 				});
 			});
 		});
